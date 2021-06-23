448e39c4fe589267facc6431486fc5c83a457e2e
tvitiello <thomas.vitiello@outlook.fr>
2021-06-23 22:52:11 +0200

    REFACTORING -- show 'SetTest' output if param key is 'Test'

--- src/Hydrator.php
+++ src/Hydrator.php
@@ -3,7 +3,10 @@
 namespace App;
 
 class Hydrator {
-    public function hydrate(){
-        
+    public function hydrate(Array $datas){
+        foreach($datas as $key => $value){
+            $method = 'set'.ucfirst($key);
+            echo $method;
+        }
     }
 }
