268ca55701f7bbc5f0a2af578f4c849bff60819a
tvitiello <thomas.vitiello@outlook.fr>
2021-06-24 00:39:43 +0200

    REFACTORING -- call \App\Database\Manager constructor with parameters

--- src/Database/Manager.php
+++ src/Database/Manager.php
@@ -2,6 +2,17 @@
 
 namespace App\Database;
 
+use PDO;
+
 class Manager {
-    
+
+    private PDO|null $dbh;
+
+    public function __construct(
+        private string $host,
+        private int $port,
+        private string $user,
+        private string $password,
+        private string $database
+    ){}
 }
