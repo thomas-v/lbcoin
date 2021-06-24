108f4638125108a25937acd1b9718d6e9c6fad65
tvitiello <thomas.vitiello@outlook.fr>
2021-06-24 22:31:27 +0200

    REFACTORING -- call App\Treatment constructor with parameters

--- src/Treatment/Manager.php
+++ src/Treatment/Manager.php
@@ -3,5 +3,11 @@
 namespace App\Treatment;
 
 class Manager {
-
+    public function __construct(
+        private int $int1,
+        private int $int2,
+        private int $limit,
+        private string $str1,
+        private string $str2
+    ){}
 }
