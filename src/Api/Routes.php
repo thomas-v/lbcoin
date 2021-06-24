6461d475e10cfe074115d76a8a6a5d1fcd7b1d3a
tvitiello <thomas.vitiello@outlook.fr>
2021-06-24 23:13:44 +0200

    treatment instanciation with params route

--- src/Api/Routes.php
+++ src/Api/Routes.php
@@ -2,8 +2,17 @@
 
 use Slim\Http\Request;
 use Slim\Http\Response;
+use App\Treatment\Manager;
 
 $app->get('/generic-fizzbuzz/{int1}/{int2}/{limit}/{str1}/{str2}', function (Request $request, Response $response, array $args) {
+    
+    $treatment = new Manager(
+        $args['int1'],
+        $args['int2'],
+        $args['limit'],
+        $args['str1'],
+        $args['str2']
+    );
 
     $response->getBody()->write("Hello, 'test'");
 
