9ddf248ec425cb07c4c37ca7b8fd8f6637977831
tvitiello <thomas.vitiello@outlook.fr>
2021-06-24 13:57:33 +0200

    REFACTORING -- get json conf files

--- src/Conf/Manager.php
+++ src/Conf/Manager.php
@@ -3,5 +3,24 @@
 namespace App\Conf;
 
 class Manager {
-    
+
+    const CONF_DIRECTORY = './conf/';
+
+    private Array $confFiles;
+
+    public function __construct(){
+        $this->getJsonConfFiles();
+    }
+
+    private function getJsonConfFiles():void {
+         $this->setConfFiles(array_slice(scandir(self::CONF_DIRECTORY), 2));
+    }
+
+    public function getConfFiles():Array {
+        return $this->confFiles;
+    }
+
+    public function setConfFiles(Array $array):void {
+        $this->confFiles = $array;
+    }
 }
