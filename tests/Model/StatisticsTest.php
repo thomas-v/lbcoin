34d30be11193651e07905390324948af1677fea5
tvitiello <thomas.vitiello@outlook.fr>
2021-06-25 18:33:08 +0200

    TEST -- check call model statistics constructor with PDO param

--- tests/Model/StatisticsTest.php
+++ tests/Model/StatisticsTest.php
@@ -5,13 +5,24 @@ namespace Tests\Model;
 use App\Model\Statistics;
 use PHPUnit\Framework\TestCase;
 
+use function App\Database\getDbh;
+
 class StatisticsTest extends TestCase {
     /**
      * @test
      * @doesNotPerformAssertions
      */
     public function insertStats(){
-        new Statistics();
+
+        $databaseManager = new \App\Database\Manager(
+            database : 'fizzbuzz',
+            port : 3306,
+            user : 'fizzbuzz',
+            password : 'fizzbuzz',
+            host : 'db'
+        );
+
+        new Statistics($databaseManager->getDbh());
     }
 
 }
