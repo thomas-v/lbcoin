557b8bcaf36b0e8d05d470ee11143c5f3cb4f21b
tvitiello <thomas.vitiello@outlook.fr>
2021-06-25 13:58:01 +0200

    error management of genericFizzBuzz

--- src/Treatment/Manager.php
+++ src/Treatment/Manager.php
@@ -2,6 +2,8 @@
 
 namespace App\Treatment;
 
+use Exception;
+
 class Manager {
 
     private string|null $strTransformed;
@@ -41,22 +43,28 @@ class Manager {
     }
 
     public function genericFizzbuzz():string {
-        $str = '';
-        for($i = 1; $i <= $this->getLimit(); $i++){
-            if ($i % $this->getInt1() == 0 && $i % $this->getInt2() == 0){
-                $str .= strval($this->getStr1().$this->getStr2());
-                continue;
+        try {
+            $str = '';
+            for($i = 1; $i <= $this->getLimit(); $i++){
+                if ($i % $this->getInt1() == 0 && $i % $this->getInt2() == 0){
+                    $str .= strval($this->getStr1().$this->getStr2());
+                    continue;
+                }
+                if ($i % $this->getInt1() == 0){
+                    $str .= strval($this->getStr1());
+                    continue;
+                }
+                if ($i % $this->getInt2() == 0){
+                    $str .= strval($this->getStr2());
+                    continue;
+                }
+                $str .= strval($i);
             }
-            if ($i % $this->getInt1() == 0){
-                $str .= strval($this->getStr1());
-                continue;
-            }
-            if ($i % $this->getInt2() == 0){
-                $str .= strval($this->getStr2());
-                continue;
-            }
-            $str .= strval($i);
+            return $str;
+        } catch (Exception $e){
+            error_log($e);
+            return null;
         }
-        return $str;
+        
     }
 }
