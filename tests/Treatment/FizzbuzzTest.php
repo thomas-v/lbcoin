bcccd624673b47738934630729ea54c873ba882b
tvitiello <thomas.vitiello@outlook.fr>
2021-06-25 13:36:47 +0200

    TEST -- generic Fizzbuzz with limit 16

--- tests/Treatment/FizzbuzzTest.php
+++ tests/Treatment/FizzbuzzTest.php
@@ -6,5 +6,18 @@ use App\Treatment\Manager;
 use PHPUnit\Framework\TestCase;
 
 class FizzbuzzTest extends TestCase {
-    
+    /**
+     * @test
+     */
+    public function genericFizzbuzzWithLimit16(){
+        $treatmentManager = new Manager(
+            int1 : 3,
+            int2 : 5,
+            limit : 16,
+            str1 : 'fizz',
+            str2 : 'buzz'
+        );
+        $this->assertEquals('12fizz4buzzfizz78fizzbuzz11fizz1314fizzbuzz16', $treatmentManager->genericFizzBuzz());
+    }
+
 }
