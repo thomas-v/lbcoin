92a41bc5a4052c34a582e06ca666e43929b92757
tvitiello <thomas.vitiello@outlook.fr>
2021-06-25 13:21:18 +0200

    Clen tests Fizzbuzz

--- tests/Treatment/FizzbuzzTest.php
+++ tests/Treatment/FizzbuzzTest.php
@@ -6,17 +6,5 @@ use App\Treatment\Manager;
 use PHPUnit\Framework\TestCase;
 
 class FizzbuzzTest extends TestCase {
-    /**
-     * @test
-     */
-    public function returnStringOfListNum1ToLimit(){
-        $treatmentManager = new Manager(
-            int1 : 3,
-            int2 : 5,
-            limit : 16,
-            str1 : 'fizz',
-            str2 : 'buzz'
-        );
-        $this->assertEquals('12345678910111213141516', $treatmentManager->genericFizzBuzz());
-    }
+    
 }
