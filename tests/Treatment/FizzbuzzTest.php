0a0487fe4bd0ea6d3f5087f757e78437aae12f3b
tvitiello <thomas.vitiello@outlook.fr>
2021-06-25 13:38:07 +0200

    TEST -- generic Fizzbuzz with limit 100

--- tests/Treatment/FizzbuzzTest.php
+++ tests/Treatment/FizzbuzzTest.php
@@ -20,4 +20,17 @@ class FizzbuzzTest extends TestCase {
         $this->assertEquals('12fizz4buzzfizz78fizzbuzz11fizz1314fizzbuzz16', $treatmentManager->genericFizzBuzz());
     }
 
+    /**
+     * @test
+     */
+    public function genericFizzbuzzWithLimit100(){
+        $treatmentManager = new Manager(
+            int1 : 3,
+            int2 : 5,
+            limit : 100,
+            str1 : 'fizz',
+            str2 : 'buzz'
+        );
+        $this->assertEquals('12fizz4buzzfizz78fizzbuzz11fizz1314fizzbuzz1617fizz19buzzfizz2223fizzbuzz26fizz2829fizzbuzz3132fizz34buzzfizz3738fizzbuzz41fizz4344fizzbuzz4647fizz49buzzfizz5253fizzbuzz56fizz5859fizzbuzz6162fizz64buzzfizz6768fizzbuzz71fizz7374fizzbuzz7677fizz79buzzfizz8283fizzbuzz86fizz8889fizzbuzz9192fizz94buzzfizz9798fizzbuzz', $treatmentManager->genericFizzBuzz());
+    }
 }
