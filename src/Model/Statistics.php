0bab218cc76888a0c4d9811254608128e636b7f3
tvitiello <thomas.vitiello@outlook.fr>
2021-06-25 18:49:36 +0200

    REFACTORING -- check insert stats via model App\Model\Statistics->pushStats()

--- src/Model/Statistics.php
+++ src/Model/Statistics.php
@@ -8,4 +8,25 @@ class Statistics {
     public function __construct(
         private PDO|null $dbh
     ){}
+
+    public function pushStats(string $api_query):void{
+        try {
+            $sql = 'SELECT * FROM fizzbuzz.stats WHERE query = :api_query';
+            $sth = $this->dbh->prepare($sql);
+            $sth->execute(array(':api_query' => $api_query));
+    
+            if(count($sth->fetchAll(PDO::FETCH_ASSOC)) === 0){
+                $sql = 'INSERT INTO fizzbuzz.stats (query) VALUES (:api_query)';
+                $sth = $this->dbh->prepare($sql);
+                $sth->execute(array(':api_query' => $api_query));
+            }
+    
+            $sql = 'UPDATE fizzbuzz.stats SET nb = nb + 1 WHERE query = :api_query';
+            $sth = $this->dbh->prepare($sql);
+            $sth->execute(array(':api_query' => $api_query));
+    
+        } catch (PDOException $e){
+            error_log($e);
+        }
+    }
 }
